a1fe7fd0af4f55980338d70f54d3b4af501f6d76
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-12-15 20:22:50 -0800

    block and tool tags

--- src/main/java/com/zenith/generator/BlockRegistryGenerator.java
+++ src/main/java/com/zenith/generator/BlockRegistryGenerator.java
@@ -1,19 +1,22 @@
 package com.zenith.generator;
 
+import com.google.common.base.Suppliers;
 import com.palantir.javapoet.CodeBlock;
 import com.zenith.extension.IBlockProperties;
 import com.zenith.mc.block.Block;
 import com.zenith.mc.block.BlockOffsetType;
+import com.zenith.mc.block.BlockTags;
 import com.zenith.mixin.AccessorBlockBehavior;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.EmptyBlockGetter;
 import net.minecraft.world.level.block.state.BlockState;
 import org.geysermc.mcprotocollib.protocol.data.game.level.block.BlockEntityType;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
+import java.util.function.Supplier;
 
 public class BlockRegistryGenerator extends RegistryGenerator<Block> {
 
@@ -21,10 +24,41 @@ public class BlockRegistryGenerator extends RegistryGenerator<Block> {
         super(Block.class, Block.class.getPackage().getName(), "BlockRegistry");
     }
 
+    private Set<net.minecraft.world.level.block.Block> getTaggedBlocks(TagKey<net.minecraft.world.level.block.Block> tag) {
+        Set<net.minecraft.world.level.block.Block> blocks = new HashSet<>();
+        BuiltInRegistries.BLOCK.getOrCreateTag(tag).stream()
+                .forEach(t -> blocks.add(t.value()));
+        return blocks;
+    }
+
+    final Supplier<Set<net.minecraft.world.level.block.Block>> AXE_MINEABLE_BLOCKS = Suppliers.memoize(() -> getTaggedBlocks(net.minecraft.tags.BlockTags.MINEABLE_WITH_AXE));
+    final Supplier<Set<net.minecraft.world.level.block.Block>> HOE_MINEABLE_BLOCKS = Suppliers.memoize(() -> getTaggedBlocks(net.minecraft.tags.BlockTags.MINEABLE_WITH_HOE));
+    final Supplier<Set<net.minecraft.world.level.block.Block>> PICKAXE_MINEABLE_BLOCKS = Suppliers.memoize(() -> getTaggedBlocks(net.minecraft.tags.BlockTags.MINEABLE_WITH_PICKAXE));
+    final Supplier<Set<net.minecraft.world.level.block.Block>> SHOVEL_MINEABLE_BLOCKS = Suppliers.memoize(() -> getTaggedBlocks(net.minecraft.tags.BlockTags.MINEABLE_WITH_SHOVEL));
+    final Supplier<Set<net.minecraft.world.level.block.Block>> SWORD_MINEABLE_BLOCKS = Suppliers.memoize(() -> getTaggedBlocks(net.minecraft.tags.BlockTags.SWORD_EFFICIENT));
+    final Supplier<Set<net.minecraft.world.level.block.Block>> NEEDS_DIAMOND_TOOL_BLOCKS = Suppliers.memoize(() -> getTaggedBlocks(net.minecraft.tags.BlockTags.NEEDS_DIAMOND_TOOL));
+    final Supplier<Set<net.minecraft.world.level.block.Block>> NEEDS_IRON_TOOL_BLOCKS = Suppliers.memoize(() -> getTaggedBlocks(net.minecraft.tags.BlockTags.NEEDS_IRON_TOOL));
+    final Supplier<Set<net.minecraft.world.level.block.Block>> NEEDS_STONE_TOOL_BLOCKS = Suppliers.memoize(() -> getTaggedBlocks(net.minecraft.tags.BlockTags.NEEDS_STONE_TOOL));
+
+    private @Nullable EnumSet<BlockTags> getZenithBlockTags(final net.minecraft.world.level.block.Block block) {
+        var set = EnumSet.noneOf(BlockTags.class);
+        if (AXE_MINEABLE_BLOCKS.get().contains(block)) set.add(BlockTags.MINEABLE_WITH_AXE);
+        if (HOE_MINEABLE_BLOCKS.get().contains(block)) set.add(BlockTags.MINEABLE_WITH_HOE);
+        if (PICKAXE_MINEABLE_BLOCKS.get().contains(block)) set.add(BlockTags.MINEABLE_WITH_PICKAXE);
+        if (SHOVEL_MINEABLE_BLOCKS.get().contains(block)) set.add(BlockTags.MINEABLE_WITH_SHOVEL);
+        if (SWORD_MINEABLE_BLOCKS.get().contains(block)) set.add(BlockTags.SWORD_EFFICIENT);
+        if (NEEDS_DIAMOND_TOOL_BLOCKS.get().contains(block)) set.add(BlockTags.NEEDS_DIAMOND_TOOL);
+        if (NEEDS_IRON_TOOL_BLOCKS.get().contains(block)) set.add(BlockTags.NEEDS_IRON_TOOL);
+        if (NEEDS_STONE_TOOL_BLOCKS.get().contains(block)) set.add(BlockTags.NEEDS_STONE_TOOL);
+        if (set.isEmpty()) return null;
+        return set;
+    }
+
     @Override
     public List<Block> buildDataList() {
         List<Block> blockList = new ArrayList<>();
         var blockRegistry = BuiltInRegistries.BLOCK;
+
         blockRegistry.forEach(block -> {
             List<BlockState> blockStates = block.getStateDefinition().getPossibleStates();
             var registryKey = blockRegistry.getKey(block);
@@ -42,9 +76,12 @@ public class BlockRegistryGenerator extends RegistryGenerator<Block> {
                 net.minecraft.world.level.block.Block.getId(blockStates.getFirst()),
                 net.minecraft.world.level.block.Block.getId(blockStates.getLast()),
                 block.defaultMapColor().id,
-                ((IBlockProperties) block.properties()).getOffsetType(),
+                ((IBlockProperties) block.properties()).dg$getOffsetType(),
                 ((AccessorBlockBehavior) block).invokeGetMaxHorizontalOffset(),
                 ((AccessorBlockBehavior) block).invokeGetMaxVerticalOffset(),
+                ((IBlockProperties) block.properties()).dg$getDestroySpeed(),
+                ((IBlockProperties) block.properties()).dg$requiresCorrectToolForDrops(),
+                getZenithBlockTags(block),
                 mcplBlockEntityType);
             blockList.add(data);
         });
@@ -53,25 +90,9 @@ public class BlockRegistryGenerator extends RegistryGenerator<Block> {
 
     @Override
     public CodeBlock dataInitializer(final Block data) {
-        if (data.blockEntityType() == null) {
-            return CodeBlock.of(
-                "new $T($L, $S, $L, $L, $L, $L, $T.$L, $Lf, $Lf, $L)",
-                Block.class,
-                data.id(),
-                data.name(),
-                data.isBlock(),
-                data.minStateId(),
-                data.maxStateId(),
-                data.mapColorId(),
-                BlockOffsetType.class,
-                data.offsetType(),
-                data.maxHorizontalOffset(),
-                data.maxVerticalOffset(),
-                null
-            );
-        }
-        return CodeBlock.of(
-            "new $T($L, $S, $L, $L, $L, $L, $T.$L, $Lf, $Lf, $T.$L)",
+
+        CodeBlock baseConstructor = CodeBlock.of(
+            "new $T($L, $S, $L, $L, $L, $L, $T.$L, $Lf, $Lf, $Lf, $L",
             Block.class,
             data.id(),
             data.name(),
@@ -83,8 +104,39 @@ public class BlockRegistryGenerator extends RegistryGenerator<Block> {
             data.offsetType(),
             data.maxHorizontalOffset(),
             data.maxVerticalOffset(),
-            BlockEntityType.class,
-            data.blockEntityType()
+            data.destroySpeed(),
+            data.requiresCorrectToolForDrops()
         );
+
+        CodeBlock blockTagsPart = CodeBlock.of("");
+
+        if (data.blockTags() != null) {
+            var builder = CodeBlock.builder()
+                .add(", $T.of(", EnumSet.class);
+            BlockTags[] tagsArray = data.blockTags().toArray(BlockTags[]::new);
+            for (int i = 0; i < tagsArray.length; i++) {
+                BlockTags tag = tagsArray[i];
+                if (i > 0) {
+                    builder.add(", ");
+                }
+                builder.add("$T.$L", BlockTags.class, tag);
+            }
+            blockTagsPart = builder.add(")").build();
+        }
+
+        CodeBlock blockEntityTypePart = CodeBlock.of("");
+        if (data.blockEntityType() != null) {
+            blockEntityTypePart = CodeBlock.of(
+                ", $T.$L",
+                BlockEntityType.class,
+                data.blockEntityType()
+            );
+        }
+
+        return baseConstructor.toBuilder()
+            .add(blockTagsPart)
+            .add(blockEntityTypePart)
+            .add(")")
+            .build();
     }
 }
--- src/main/java/com/zenith/generator/ItemRegistryGenerator.java
+++ src/main/java/com/zenith/generator/ItemRegistryGenerator.java
@@ -2,36 +2,88 @@ package com.zenith.generator;
 
 import com.palantir.javapoet.CodeBlock;
 import com.zenith.mc.item.ItemData;
+import com.zenith.mc.item.ToolTag;
+import com.zenith.mc.item.ToolTier;
+import com.zenith.mc.item.ToolType;
 import net.minecraft.core.DefaultedRegistry;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.item.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class ItemRegistryGenerator extends RegistryGenerator<ItemData> {
     public ItemRegistryGenerator() {
         super(ItemData.class, ItemData.class.getPackage().getName(), "ItemRegistry");
     }
 
+    private static final Map<Tiers, ToolTier> tierMap = Map.of(
+        Tiers.WOOD, ToolTier.WOOD,
+        Tiers.STONE, ToolTier.STONE,
+        Tiers.IRON, ToolTier.IRON,
+        Tiers.DIAMOND, ToolTier.DIAMOND,
+        Tiers.GOLD, ToolTier.GOLD,
+        Tiers.NETHERITE, ToolTier.NETHERITE
+    );
+
     @Override
     public List<ItemData> buildDataList() {
         final List<ItemData> items = new ArrayList<>();
         DefaultedRegistry<Item> registry = BuiltInRegistries.ITEM;
-        registry.stream().forEach(item -> items.add(new ItemData(
-            registry.getId(item),
-            registry.getKey(item).getPath(),
-            item.getDefaultMaxStackSize())
-        ));
+
+        registry.stream().forEach(item -> {
+            ToolType toolType = null;
+            ToolTier toolTier = null;
+            if (item instanceof AxeItem i) {
+                toolType = ToolType.AXE;
+                toolTier = tierMap.get((Tiers) i.getTier());
+            } else if (item instanceof HoeItem i) {
+                toolType = ToolType.HOE;
+                toolTier = tierMap.get((Tiers) i.getTier());
+            } else if (item instanceof PickaxeItem i) {
+                toolType = ToolType.PICKAXE;
+                toolTier = tierMap.get((Tiers) i.getTier());
+            } else if (item instanceof ShovelItem i) {
+                toolType = ToolType.SHOVEL;
+                toolTier = tierMap.get((Tiers) i.getTier());
+            } else if (item instanceof SwordItem i) {
+                toolType = ToolType.SWORD;
+                toolTier = tierMap.get((Tiers) i.getTier());
+            }
+            ToolTag toolTag = null;
+            if (toolType != null && toolTier != null) {
+                toolTag = new ToolTag(toolTier, toolType);
+            }
+            items.add(new ItemData(
+                registry.getId(item),
+                registry.getKey(item).getPath(),
+                item.getDefaultMaxStackSize(),
+                toolTag)
+            );
+        });
         return items;
     }
 
     @Override
     public CodeBlock dataInitializer(final ItemData item) {
-        return CodeBlock.of("new $T($L, $S, $L)",
-                            this.dataType,
-                            item.id(),
-                            item.name(),
-                            item.stackSize());
+        if (item.toolTag() == null) {
+            return CodeBlock.of("new $T($L, $S, $L)",
+                                this.dataType,
+                                item.id(),
+                                item.name(),
+                                item.stackSize());
+        } else {
+            return CodeBlock.of("new $T($L, $S, $L, new $T($T.$L, $T.$L))",
+                                this.dataType,
+                                item.id(),
+                                item.name(),
+                                item.stackSize(),
+                                ToolTag.class,
+                                ToolTier.class,
+                                item.toolTag().tier(),
+                                ToolType.class,
+                                item.toolTag().type());
+        }
     }
 }
