b129cf9c4b11cca1adc45bb76b0780b8fb519c09
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-05-08 17:25:48 -0700

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ loom {
         }
     }
 }
-val lombokVersion = "1.18.36"
+val lombokVersion = "1.18.38"
 
 dependencies {
     minecraft("com.mojang:minecraft:1.21.5")
@@ -38,10 +38,10 @@ dependencies {
     modImplementation("net.fabricmc:fabric-loader:0.16.10")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.119.5+1.21.5")
     implementation("com.palantir.javapoet:javapoet:0.6.0")
-    implementation("com.github.rfresh2:MCProtocolLib:1.21.5.1") {
+    implementation("com.github.rfresh2:MCProtocolLib:1.21.5.10") {
         isTransitive = false
     }
-    implementation("com.zenith:ZenithProxy:1.21.4-SNAPSHOT") {
+    implementation("com.zenith:ZenithProxy:1.21.5-SNAPSHOT") {
         isTransitive = false
     }
     compileOnly("org.projectlombok:lombok:$lombokVersion")
