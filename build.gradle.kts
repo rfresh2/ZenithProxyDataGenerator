86d382087de8623debed61a035cd4dae66bde4c6
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-05-21 16:53:43 -0700

    dynamic registry generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     modImplementation("net.fabricmc:fabric-loader:0.16.10")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.100.1+1.21")
     implementation("com.palantir.javapoet:javapoet:0.7.0")
-    implementation("com.github.rfresh2:MCProtocolLib:1.21.0.30") {
+    implementation("com.github.rfresh2:MCProtocolLib:1.21.0.48") {
         isTransitive = false
     }
     implementation("com.zenith:ZenithProxy:1.21.0-SNAPSHOT") {
