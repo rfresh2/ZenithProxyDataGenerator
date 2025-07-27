dbbf242c9001a122599d3f3f538b6ea547402f9d
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-07-26 20:45:29 -0700

    ageable mob entity data

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,10 +32,10 @@ dependencies {
         officialMojangMappings()
         parchment("org.parchmentmc.data:parchment-1.21.4:2025.03.16@zip")
     })
-    modImplementation("net.fabricmc:fabric-loader:0.16.10")
+    modImplementation("net.fabricmc:fabric-loader:0.16.14")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.110.5+1.21.4")
     implementation("com.palantir.javapoet:javapoet:0.7.0")
-    implementation("com.github.rfresh2:MCProtocolLib:1.21.4.23") {
+    implementation("com.github.rfresh2:MCProtocolLib:1.21.4.26") {
         isTransitive = false
     }
     implementation("org.cloudburstmc.math:immutable:2.0")
