70f3ab55579dae0b4f2f452f52ac63a52d7be577
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-08-02 19:55:58 -0700

    entity blocks building data gen

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ group = "com.zenith"
 version = "1.0.0"
 
 repositories {
-//    mavenLocal()
+    mavenLocal()
     maven("https://maven.parchmentmc.org")
     maven("https://maven.2b2t.vc/releases")
     maven("https://maven.2b2t.vc/remote")
