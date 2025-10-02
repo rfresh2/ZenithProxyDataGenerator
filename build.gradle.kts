9d385ae7bd849510f1d5b5bcc9a53460e532ddd0
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-10-01 23:49:12 -0700

    update floader

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
         officialMojangMappings()
         parchment("org.parchmentmc.data:parchment-1.21.4:2025.03.23@zip")
     })
-    modImplementation("net.fabricmc:fabric-loader:0.16.14")
+    modImplementation("net.fabricmc:fabric-loader:0.17.2")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.119.4+1.21.4")
     implementation("com.palantir.javapoet:javapoet:0.7.0")
     implementation("com.github.rfresh2:MCProtocolLib:1.21.4.28") {
