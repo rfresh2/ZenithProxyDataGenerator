088ae0c711fe1ebeb1b4434745b3ab8130daea70
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-10-01 23:47:30 -0700

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("fabric-loom") version "1.10-SNAPSHOT"
+    id("fabric-loom") version "1.11-SNAPSHOT"
 }
 
 group = "com.zenith"
@@ -33,7 +33,7 @@ dependencies {
         parchment("org.parchmentmc.data:parchment-1.21.4:2025.03.23@zip")
     })
     modImplementation("net.fabricmc:fabric-loader:0.16.14")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.110.5+1.21.4")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.119.4+1.21.4")
     implementation("com.palantir.javapoet:javapoet:0.7.0")
     implementation("com.github.rfresh2:MCProtocolLib:1.21.4.28") {
         isTransitive = false
